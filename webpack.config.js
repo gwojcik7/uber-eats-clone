586cba751441f955853aa730e72912c6cd2fcc35
Grzegorz Wójcik <w57104@student.wsiz.edu.pl>
2021-09-22 19:53:17 +0200

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let config = {
+    entry: {
+        style: ['./src/scss/style.scss'],
+        script: ['./src/js/script.js'],
+    },
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'js/[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(png|jpg|gif|woff|woff2|ttf|eot|svg)$/,
+                loader: 'ignore-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            url: false,
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            config: {
+                                path: 'postcss.config.js',
+                            },
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    externals: {
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+        }),
+    ],
+    devtool: 'source-map',
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+};
+
+module.exports = config;
